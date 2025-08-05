8fc9711fe43df004d899262a4cc934dfcd6be557
Sandy3395 <suman.sandeep1999@gmail.com>
2025-08-05 19:49:21 +0530

    updated padding

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex-container">
-       <div className="header">Header</div>
+       <div className="header"><span style={{ padding:'7px' }}>Header</span></div>
        <div className="middle">
-         <div className="article">article</div>
-         <div className="aside">Aside</div>
-         <div className="nav">Nav</div>
+         <div className="article"><span style={{ padding:'7px' }}>article</span></div>
+         <div className="aside"><span style={{ padding:'7px' }}>Aside</span></div>
+         <div className="nav"><span style={{ padding:'7px' }}>Nav</span></div>
        </div>
-       <div className="footer">footer</div>
+       <div className="footer"><span style={{ padding:'7px' }}>footer</span></div>
     </div>
   );
 }
