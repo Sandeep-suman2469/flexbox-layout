45f589de74d960e8320b6269ea5f760068185168
Sandy3395 <suman.sandeep1999@gmail.com>
2025-08-05 19:21:51 +0530

    width changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
        <div className="header">Header</div>
        <div className="middle">
          <div className="article">article</div>
-         <div className="nav">Aside</div>
-         <div className="aside">Nav</div>
+         <div className="aside">Aside</div>
+         <div className="nav">Nav</div>
        </div>
        <div className="footer">footer</div>
     </div>
